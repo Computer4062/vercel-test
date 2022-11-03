b4e88c6cd55328e105c4093e8d881310efdb3127
Mihan <87608466+Computer4062@users.noreply.github.com>
2022-11-03 20:54:28 +0400

    commit
    commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use('/products', product)
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('views/home_page')
+    res.render('home_page')
 })
 
 
--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const Router = express()
+const Router = express.Router()
 
 Router.get('/', (req, res) => {
     res.send("Working")
